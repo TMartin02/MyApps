a100abd463228c07dba3d0f18b57a8c111380136
TMartin02 <tomekmartin3@gmail.com>
2019-03-06 12:08:53 +0100

    #2: játékos válasz bekérése

--- kpo/kpo/Program.cs
+++ kpo/kpo/Program.cs
@@ -11,7 +11,7 @@ namespace kpo
         static void Main(string[] args)
         {
 
-
+            
             ///Program: Kő papír olló játék leprogramozása
             ///Irta: Tomek Martin / 2019.03.06
             ///Adott: számítógép - véletlenszerű válasz
@@ -21,17 +21,43 @@ namespace kpo
 
             bool moreGame = true;
 
-            ///Feladat
+            ///Feladat változók létrehozása
+            
+            string compChoice = "";
+            string playerChoice = "";
+
+            int compScore = 0;
+            int playerScore = 0;
+        
             Console.WriteLine("**********Kő/Papír/Olló játék**********");
 
             do
             {
+                ///Feladat 3:
+                ///Player választásának megkérdezése és tárolása
+                
+                Console.WriteLine("Te mitválasztasz?");
+
+                switch (Console.ReadKey(true).KeyChar)
+                    {
+                    case 'k'..
+                    playerChoice = "kő";
+                    break;
+                    case'p'..
+                    playerChoice = "papír";
+                    break;
+                    case 'o'..
+                    playerChoice = "olló";
+                    break;
+                    }
                 Console.Write("Akarsz még játszani? i/n");
                 if (Console.ReadKey(true).KeyChar == 'n')
                 {
                     moreGame = false;
                 }
             } while (moreGame);
+            
+
             Console.ReadKey();
 
            

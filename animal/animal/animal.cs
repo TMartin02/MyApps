75e71c5b4deb01032b62fbb581152bb370f7b5dc
TMartin02 <tomekmartin3@gmail.com>
2019-04-03 13:07:13 +0200

    #5: cicamica kölyök + kutya

---
+++ animal/animal/animal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animal
+{
+   abstract class animal
+    {
+        protected string Name;
+        abstract public void Sleep();
+        abstract public void Hunt();
+        abstract public void Sound();
+
+    }
+    //Feladat: adjunk a macskához lehetőséget hogy kölyke legyen
+    class Cat : animal
+    {
+        private List<Cat> Kids;
+
+        public Cat(string aName)
+        {
+            Name = aName;
+        }
+        internal void Born(int v)
+        {
+            Kids = new List<Cat>();
+            for (int i = 0; i < v; i++)
+            {
+                Kids.Add(new Cat("cica" + i.ToString()));
+            }
+        }
+        internal void Child()
+        {
+            foreach (var item in Kids)
+            {
+                Console.WriteLine(Name + " s child name:" + item.Name);
+            }
+        }
+        public Cat()
+        {
+
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine(Name + " is sleeping......");
+        }
+        public override void Hunt()
+        {
+            Console.WriteLine(Name + " is hunting");
+        }
+        public override void Sound()
+        {
+            Console.WriteLine(Name + " is nyávog");
+        }
+              
+    }
+    class Dog : animal
+    {
+        public Dog(string aName)
+        {
+            Name = aName;
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine(Name + " is sleeping......");
+        }
+        public override void Hunt()
+        {
+            Console.WriteLine(Name + " is hunting");
+        }
+        public override void Sound()
+        {
+            Console.WriteLine(Name + " is Sound");
+        }
+    }
+}

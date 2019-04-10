69e6975bc392db5017ad8d1d0825947c66608d93
TMartin02 <tomekmartin3@gmail.com>
2019-04-10 12:15:38 +0200

    #6 : töbdimenziós tömbök

--- tipuskonverzio/tipuskonverzio/Program.cs
+++ tipuskonverzio/tipuskonverzio/Program.cs
@@ -8,6 +8,8 @@ namespace tipuskonverzio
 {
     class Program
     {
+        private static int iPrev;
+
         static void Main(string[] args)
         {
             int x = 10;
@@ -31,8 +33,6 @@ namespace tipuskonverzio
 
             string cc = c as string;
             Console.WriteLine(cc == null ? "NULL" : cc); // NULL 
-
-            int x = 10;
             Console.WriteLine(x == 5 ? "x ==5 " : "x != 5");
 
             for (char ch = 'a'; ch <= 'z'; ch++)
@@ -45,15 +45,41 @@ namespace tipuskonverzio
             for (int i = 0; i < iTomb.Length; i++)
             {
                 //iTomb[i] = r.next();
-                
+                Console.Write("Kérem a {0}. számot:", i);
+                try
+                {
+                    iTomb[i] = int.Parse(Console.ReadLine());
+                    iPrev = i;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Hiba." + e.Message);
+                    i=iPrev;
+                }
+                                                        
+
             }
             for (int i = 0; i < iTomb.Length; i++)
             {
                 Console.Write(iTomb[i] + " ");
             }
 
-                                             
+            int[] iArr = new int[] { 1, 2, 3, 4 };
+            string[] sArr = new string[] { "banán,mangó,alma,körte" };
 
+            Array.Sort(sArr);
+            for (int i = 0; i < sArr.Length; i++)
+            {
+                Console.Write(sArr[i] + " ");
+            }
+
+            //több dimenziós tömb
+            int[,] matrix = new int[,]
+            {
+                {12, 23, 2},
+                {13, 67, 52},  
+                {45, 55, 1}
+            };
             Console.ReadKey();
 
         }

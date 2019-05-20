c87256d5b1ebdd322e2edf21cf7b9f6de933b509
TMartin02 <tomekmartin3@gmail.com>
2019-05-20 11:20:36 +0200

    0

---
+++ counter 03/counter 03/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace counter_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string betuk;
+            string q,w,e,r,t,z,u,i,o,p,a,s,d,í,f,g,h,j,k,l,y,c,x,v,b,n,ö,ü,ó,ő,ú,é,á,ű,m;
+
+            Console.WriteLine("Kérem a szöveget:");
+            betuk = (Console.ReadLine());
+
+            Console.ReadKey();
+                      
+        }
+    }
+}

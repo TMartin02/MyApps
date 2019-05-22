a5605d3a6e864d643d3be238d5f3e9646e23199b
TMartin02 <tomekmartin3@gmail.com>
2019-05-22 11:19:00 +0200

    prím

--- lab 05 20/lab 05 20/Program.cs
+++ lab 05 20/lab 05 20/Program.cs
@@ -10,43 +10,51 @@ namespace lab_05_20
     {
         static void Main(string[] args)
         {
-            int[] iArray = new int[15];
-            string[] sArray = new string[5];
-            int[] iT = new int[] { 5, 8, 9 };
-            int[][] iT2 = new int[][]
-            {
-                new int[] { 4,5},
-                new int[] { 2,3},
-                new int[] { 6,8},
-            };
-            int[,] iT3 = new int[,] { { 4, 3 }, { 2, 3 }, { 6, 8 } };
-            try
-            {
-                Console.WriteLine(iT3[3, 1]);
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+         
 
-            int[,,] iT4 = new int[,,] { { { 1, 2, 3 }, { 4,5,6} } ,
-                                  {{7,8,9},{10,11,12} } };
-            try
+
+
+            int[] szamok = new int[30];
+            Random rnd = new Random();
+            int min = 1, max = 100, i, j, k, m, n;
+            bool b = true;
+            for (i = 0; i < 30; i++)
             {
-                Console.WriteLine(iT4[1, 1, 0]);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            for (int i = 0; i < 50; i++)
-            {
-                if ((i % 3) == 0)
+                do
                 {
-                    Console.WriteLine(i);
-                }
+                    j = i - 1;
+                    k = rnd.Next(min, max);
+                    while (j > -1)
+                    {
+                        if (k == szamok[j])
+                        {
+                            k = 0;
+                            j = 0;
+                        }
+                        j--;
+                    }
+                    {
+                        szamok[i] = k;
+                        b = true;
+                        if (k != 2)
+                        {
+                            if (k < 2 || (k % 2) == 0) b = false;
+                            else
+                            {
+                                m = 3;
+                                n = (int)Math.Sqrt(k);
+                                while (m <= n && b)
+                                {
+                                    if ((k % m) == 0) b = false;
+                                    m += 2;
+                                }
+                            }
+                        }
+                    }
+                } while (k == 0);
+                Console.WriteLine(k + ":    " + (b ? "Prímszám." : "Nem Prímszám."));
             }
-
+            Console.ReadLine();
 
 
             Console.ReadKey();
